61e93ce6cd2fb213471fb76c973497008adfa1ec
UdheySingh <56387009+UdheySingh@users.noreply.github.com>
2020-12-06 19:28:58 +0100

    Add files via upload

---
+++ PuntiVendita.java
@@ -0,0 +1,28 @@
+package com.mycompany.javahttpserver;
+
+import java.util.List;
+
+public class PuntiVendita 
+{
+    private int size;
+    private List<PuntoVendita> listaRisultati;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) 
+    {
+        this.size = size;
+    }
+
+    public List<PuntoVendita> getListaRisultati() 
+    {
+        return listaRisultati;
+    }
+
+    public void setListaRisultati(List<PuntoVendita> listaRisultati) 
+    {
+        this.listaRisultati = listaRisultati;
+    }
+}
